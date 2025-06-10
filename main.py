c05be5b09bb91926d255b140ccd525f4b4021ace
edugitqa <eduardo.telecomrbs@gmail.com>
2025-06-09 22:15:37 -0300

    Projeto inicial: Langchain FAISS QA

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
-from langchain_documents_loader import TextLoader
-from langchain.text_splitter import CaracterTextSplitter
-from lanchain.vectorstores import FAISS
-from lanchain.embeddings import OpenAIEmbeddings
-from langchain.questions_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+
+#aqui eu carrego o doc
+loader = TextLoader("./doc/exemplo.txt")
+documents = loader.load()
+
+#divide o texto em pedaços
+TextLoader_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs = TextLoader_splitter.split_documents(documents)
+
+#trabalhar com embeddings E BANCO DE DADOS
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(docs, embeddings)
+
+chain = load_qa_chain(llm, chain_type="stuff")
+
+while True:
+    pergunta = input("Digite sua pergunta sobre o documento(Ou 'sair' para encerrar): ")
+    if pergunta.lower() == 'sair':
+        break
+    docs_relevantes = db.similarity_search(pergunta)
+    resposta = chain.run(input_documents=docs_relevantes, question=pergunta)
+    print("\nResposta: ", resposta)   
+
+
+
+docs_relevantes = db.similarity_search(pergunta)
+
+
+
+resposta = chain.run(input_documents=docs_relevantes, question=pergunta)
+
+print("\nResposta: ", resposta)
+
 
